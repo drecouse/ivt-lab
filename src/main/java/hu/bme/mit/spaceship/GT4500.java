71f5f3cb49b1361137278a08beab1cedba6121ad
Your Name <you@example.com>
2018-05-04 02:04:23 -0700

    Fixed missing name before SINGLE and ALL

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,7 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    if(firingMode == SINGLE){
+    if(firingMode == FiringMode.SINGLE){
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,7 +74,7 @@ public class GT4500 implements SpaceShip {
           }
         }
     }
-    else if (firingMode == ALL){     
+    else if (firingMode == FiringMode.ALL){     
         // try to fire both of the torpedo stores
 	if (!primaryTorpedoStore.isEmpty()){
 		firingSuccess = primaryTorpedoStore.fire(1);
